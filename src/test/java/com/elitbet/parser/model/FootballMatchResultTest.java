82b370a7d82f6ac76eea2b6657a6bf6a18ae326d
Roman <romanchi40160@gmail.com>
2018-11-02 19:42:16 +0200

    -deleted deleted unused imports

--- src/test/java/com/elitbet/parser/model/FootballMatchResultTest.java
+++ src/test/java/com/elitbet/parser/model/FootballMatchResultTest.java
@@ -1,10 +1,6 @@
 package com.elitbet.parser.model;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
--- src/test/java/com/elitbet/parser/model/FootballMatchTest.java
+++ src/test/java/com/elitbet/parser/model/FootballMatchTest.java
@@ -1,11 +1,5 @@
 package com.elitbet.parser.model;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
